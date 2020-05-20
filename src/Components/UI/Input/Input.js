88b681745392aefdf548d37d8cdc63df6f3cfe37
Kesa Shabarish <shabarish.shabbi@gmail.com>
2020-05-20 00:23:20 -0400

    added form and validations

---
+++ src/Components/UI/Input/Input.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import classes from './Input.css';
+
+const input = (props) => {
+    let inputElement = null;
+    const inputClasses = [classes.InputElement]
+    if(props.valid && props.touched) {
+        inputClasses.push(classes.Invalid)
+    }
+
+    switch(props.elementType) {
+        case('input'):
+            inputElement = 
+            <input className = {inputClasses.join(' ')} 
+            {...props.elementConfig} 
+            value = {props.value}
+            onChange = {props.inputChanged}>
+            </input>;
+            break;
+        case('textarea'):
+            inputElement = 
+            <textarea className = {inputClasses.join(' ')} 
+            {...props.elementConfig} 
+            value = {props.value}
+            onChange = {props.inputChanged}>
+            </textarea>;
+            break;
+        case('select'):
+            inputElement = (
+            <select className = {classes.InputElement}  
+            value = {props.value}
+            onChange = {props.inputChanged}>
+                {props.elementConfig.options.map(option => {
+                    return (<option key = {option.value} 
+                        value = {option.value}>
+                        {option.displayValue}
+                        </option>)
+                })}
+            </select>
+            );
+            break;
+        default:
+            inputElement = 
+            <input className = {inputClasses.join(' ')}
+            {...props.elementConfig} 
+            value = {props.value}
+            onChange = {props.inputChanged}>
+            </input>
+    }
+    return(
+        <div className = {classes.Input}>
+            <label className = {classes.Label}>{props.label}</label>
+            {inputElement}
+        </div>
+    )
+};
+
+export default input;
